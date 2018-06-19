307ee7534166d29a44a8fb0dc4f149b544fee138
RDShah <Lntshah@yahoo.com>
2018-06-19 13:02:04 -0400

    ball and constants

---
+++ ball.py
@@ -0,0 +1,20 @@
+from constants import dt
+import numpy as np
+
+class Ball:
+	def __init__(self):
+		self.position = np.zeros(2)
+		self.acceleration = np.zeros(2)
+		self.velocity = np.zeros(2)
+		self.is_in_possession = False
+
+	def set_acceleration(self,acc):
+		assert self.is_in_possession or (acc==np.zeros(2)).all(),'No one has possession of the ball, it cannot accelerate.'
+		self.acceleration = acc
+
+	def step(self):
+		self.position += dt*self.velocity
+		self.velocity += dt*self.acceleration
+
+	def get_summary(self):
+		return {'position':self.position,'velocity':self.velocity}
